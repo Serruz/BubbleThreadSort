d67db6dfe29ceba39109545a5b5ac69bd283e019
Serruz <serruz001@gmail.com>
2017-03-23 09:33:07 +0300

    Add files via upload

---
+++ BubbleThreadSort/BubbleThreadSort/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleThreadSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов массива: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            int[] array = new int[quantity];
+            for (int i = 0; i < quantity; i++)
+            {
+                array[i] = rand.Next(100);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("1 для обычной сортировки, 2 для сортировки с потоками, 3 для ThreadPool ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            switch (b)
+            {
+                case 1:
+                    Console.WriteLine(DateTime.Now.ToString("hh:mm:ss:fff"));
+                    Sort.Bubble_Sort(array);
+                    Console.WriteLine(DateTime.Now.ToString("hh:mm:ss:fff"));
+                    Console.WriteLine("Обычная");
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        Console.Write(array[i] + " ");
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine(DateTime.Now.ToString("hh:mm:ss:fff"));
+                    Sort.ThreadBubbleSort(array);
+                    Console.WriteLine(DateTime.Now.ToString("hh:mm:ss:fff"));
+                    Console.WriteLine("Параллельно");
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        Console.Write(array[i] + " ");
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine(DateTime.Now.ToString("hh:mm:ss:fff"));
+                    Sort.ThreadPoolBubbleSort(array);
+                    Console.WriteLine(DateTime.Now.ToString("hh:mm:ss:fff"));
+                    Console.WriteLine("С пулом");
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        Console.Write(array[i] + " ");
+                    }
+                    break;
+
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BubbleThreadSort/BubbleThreadSort/Sort.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace BubbleThreadSort
+{
+    public static class Sort
+    {
+        public static void Bubble_Sort(int[] array)
+        {
+            for (int k = array.Length - 1; k > 0; k--)
+                for (int i = 0; i < k; i++)
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(ref array[i], ref array[i + 1]);
+                    }
+
+        }
+
+        public static void Swap(ref int aFirstArg, ref int aSecondArg)
+        {
+            int tmpParam = aFirstArg;
+            aFirstArg = aSecondArg;
+            aSecondArg = tmpParam;
+        }
+
+        public static void ThreadBubbleSort(int[] array)
+        {
+            //Thread thread = new Thread((id) =>                 
+            Thread[] thr1 = new Thread[(int)array.Length / 2];
+            Thread[] thr2 = new Thread[(int)array.Length / 2];
+            int count1;
+            int count2;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.WriteLine("Начало сортировки с прохода {0}", i);
+                if (i % 2 == 0)//если i четное
+                {
+                    count1 = 0; ;
+                    for (int j = 0; j < array.Length / 2; j++)
+                    {
+                        int temp = j;
+                        count1++;
+                        thr1[temp] = new Thread(() =>
+                          {
+                              Console.WriteLine("Поток1 {0} Начал сортировку", temp);
+                              if (array[2 * temp] > array[2 * temp + 1])
+                              {
+                                  Swap(ref array[2 * temp], ref array[2 * temp + 1]);
+                              }
+                              Console.WriteLine("Поток1 {0} закончил сортировку", temp);
+                          });
+                        thr1[temp].Start();
+                    }
+                    for (int m = 0; m < count1; m++)
+                    {
+                        thr1[m].Join();
+                    }
+                }
+                else//если i нечётное
+                {
+                    count2 = 0;
+                    for (int k = 0; k < array.Length / 2 - 1; k++)
+                    {
+                        int temp = k;
+                        count2++;
+                        thr2[temp] = new Thread(() =>
+                          {
+                              Console.WriteLine("Поток2 {0} Начал сортировку", temp);
+                              if (array[2 * temp + 1] > array[2 * temp + 2])
+                              {
+                                  Swap(ref array[2 * temp + 1], ref array[2 * temp + 2]);
+                              }
+                              Console.WriteLine("Поток2 {0} закончил сортировку", temp);
+                          });
+                        thr2[temp].Start();
+                    }
+                    for (int m = 0; m < count2; m++)
+                    {
+                        thr2[m].Join();
+                    }
+                }
+
+            }
+
+
+        }
+
+        public static void ThreadPoolBubbleSort(int[] array)
+        {
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < array.Length / 2; j++)
+                    {
+                        int temp = j;
+                        ThreadPool.QueueUserWorkItem(new WaitCallback((s) =>
+                        {
+                            if (array[2 * temp] > array[2 * temp + 1])
+                            {
+                                Swap(ref array[2 * temp], ref array[2 * temp + 1]);
+                            }
+                        }));
+                    }
+                }
+                else
+                {
+                    for (int k = 0; k < array.Length / 2 - 1; k++)
+                    {
+                        int temp = k;
+                        ThreadPool.QueueUserWorkItem(new WaitCallback((s) =>
+                        {
+                            if (array[2 * temp + 1] > array[2 * temp + 2])
+                            {
+                                Swap(ref array[2 * temp + 1], ref array[2 * temp + 2]);
+                            }
+                        }));
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
